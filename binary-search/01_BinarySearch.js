211053299c3f61ef134ea015808f17ea5531b86a
Art <ya.artriches@ya.ru>
2019-08-04 16:23:55 +0300

    Add Binary search

--- binary-search/01_BinarySearch.js
+++ binary-search/01_BinarySearch.js
@@ -1,1 +1,22 @@
-console.log('hello!');
+var sortedArray = [1, 3, 5, 7, 8, 10];
+var targetValue = 3;
+
+function binarySearch (array, value) {
+    let min = 0;
+    let max = array.length-1;
+
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        console.log(min + ' ' + max + ' | ' + mid);
+        if (value === array[mid]) {
+            return console.log('Искомое число ' + value + ', на позиции ' + mid);
+        } else if (value < array[mid]) {
+            max = mid;
+        } else {
+            min = mid;
+        }
+
+    }
+}
+
+binarySearch(sortedArray, targetValue);
