1f3abe3261a2514759a19fa832351260c1e2e629
Art <ya.artriches@ya.ru>
2019-08-07 19:06:23 +0300

    Add Linear search

---
+++ linear-search/01_LinearSearch.js
@@ -0,0 +1,24 @@
+var sortedArray = [1, 2, 4, 5, 5, 8, 14, 15, 20];
+var targetValue = 16;
+
+function binarySearch (array, value) {
+    let first = 0;
+    let last = array.length-1;
+
+    while (first <= last) {
+        let mid = Math.floor(first + (last - first) / 2);
+        
+        //для просмотра изменнеий на каждом шаге поиска
+        //console.log(first + ' ' + last + ' | ' + mid);
+        
+        if (value === array[mid]) {
+            return console.log('Искомое число ' + value + ', на позиции ' + mid);
+        } else if (value < array[mid]) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
+        }
+    }
+}
+
+binarySearch(sortedArray, targetValue);
