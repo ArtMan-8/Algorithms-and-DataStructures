b5f471956472aca9729ef709275955b104e66af0
Art <ya.artriches@ya.ru>
2019-08-10 11:02:54 +0300

    Add Selection search

---
+++ selection-search/01_SelectionSearch.js
@@ -0,0 +1,27 @@
+var unsortedArray = [1, 21, 5, 17, 19, 2, 14, 10, 23];
+
+function selectionSearch (array) {
+    for (let min = 0; min < array.length - 1; min++) {
+        let least = min;
+
+        for (let current = min + 1; current < array.length; current++) {
+            console.log('Индексы переборов - ' + min + ' ' + current);
+            console.log('Массив до обмена - ' + array);
+            if (array[current] < array[least]) {
+                least = current;
+                console.log('Найдено наименьшее ' + array[least] + ', на позиции ' + least);
+            }
+            console.log('---------------');
+        }
+
+        let tmp = array[min];
+        array[min] = array[least];
+        array[least] = tmp;
+        console.log('Массив после обмена - ' + array);
+        console.log('================');
+    }
+
+    return console.log('Сортированный массив - ' + array);
+}
+
+selectionSearch(unsortedArray);
